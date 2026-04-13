47cf0058187ee96700f0bbc4ef8831141c06ae97
optimizeriklanin-dot <optimizeriklanin@gmail.com>
2026-04-14 02:46:36 +0700

    Add files via upload

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export default function proxy(request: NextRequest) {
   const isAuth = request.cookies.has('iklanin_admin_auth')
   const isLoginPage = request.nextUrl.pathname === '/login'
 
